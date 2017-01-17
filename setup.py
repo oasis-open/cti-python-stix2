86585d229e2c0071472b36529246a1f4c72c56ed
Greg Back <gback@mitre.org>
2017-01-17 08:54:39 -0800

    Initial package files.

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name='stix2',
+    description="Produce and consume STIX 2 JSON content",
+    version='0.0.1',
+    packages=find_packages(),
+    keywords="stix stix2 json cti cyber threat intelligence",
+)
