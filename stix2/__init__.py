3e7adef792d586ed027011ba2d6d0906ee03ef8e
Greg Back <gback@mitre.org>
2017-01-17 16:53:27 -0800

    Add Malware object with required fields.

--- stix2/__init__.py
+++ stix2/__init__.py
@@ -7,14 +7,19 @@ import pytz
 
 
 def format_datetime(dt):
+    # TODO: how to handle naive datetime
+
     # 1. Convert to UTC
     # 2. Format in isoformat
     # 3. Strip off "+00:00"
     # 4. Add "Z"
+
+    # TODO: how to handle timestamps with subsecond 0's
     return dt.astimezone(pytz.utc).isoformat()[:-6] + "Z"
 
 
-class Indicator(collections.Mapping):
+class _STIXBase(collections.Mapping):
+    """Base class for STIX object types"""
 
     def __getitem__(self, key):
         return self._inner[key]
@@ -32,7 +37,10 @@ class Indicator(collections.Mapping):
     def __setattr__(self, name, value):
         if name != '_inner':
             raise ValueError("Cannot modify properties after creation.")
-        super(Indicator, self).__setattr__(name, value)
+        super(_STIXBase, self).__setattr__(name, value)
+
+
+class Indicator(_STIXBase):
 
     def __init__(self, type='indicator', id=None, created=None, modified=None,
                  labels=None, pattern=None, valid_from=None):
@@ -67,7 +75,7 @@ class Indicator(collections.Mapping):
 
         self._inner = {
             'type': type,
-            'id': id or now,
+            'id': id,
             'created': created or now,
             'modified': modified or now,
             'labels': labels,
@@ -85,4 +93,56 @@ class Indicator(collections.Mapping):
             'labels': self['labels'],
             'pattern': self['pattern'],
             'valid_from': format_datetime(self['valid_from']),
+        }, indent=4, sort_keys=True, separators=(",", ": "))  # Don't include spaces after commas.
+
+
+class Malware(_STIXBase):
+
+    def __init__(self, type='malware', id=None, created=None, modified=None,
+                 labels=None, name=None):
+        # TODO:
+        # - created_by_ref
+        # - revoked
+        # - external_references
+        # - object_marking_refs
+        # - granular_markings
+
+        # - description
+        # - kill_chain_phases
+
+        if not created or not modified:
+            now = datetime.now(tz=pytz.UTC)
+
+        if type != 'malware':
+            raise ValueError("Malware must have type='malware'.")
+
+        if not id:
+            id = 'malware--' + str(uuid.uuid4())
+        if not id.startswith('malware--'):
+            raise ValueError("Malware id values must begin with 'malware--'.")
+
+        if not labels:
+            raise ValueError("Missing required field for Malware: 'labels'.")
+
+        if not name:
+            raise ValueError("Missing required field for Malware: 'name'.")
+
+        self._inner = {
+            'type': type,
+            'id': id,
+            'created': created or now,
+            'modified': modified or now,
+            'labels': labels,
+            'name': name,
+        }
+
+    def __str__(self):
+        # TODO: put keys in specific order. Probably need custom JSON encoder.
+        return json.dumps({
+            'type': self['type'],
+            'id': self['id'],
+            'created': format_datetime(self['created']),
+            'modified': format_datetime(self['modified']),
+            'labels': self['labels'],
+            'name': self['name'],
         }, indent=4, sort_keys=True, separators=(",", ": "))  # Don't include spaces after commas.
