5e4ca9e882417f95819e8b1d87657bd0182a6e93
clenk <clenk@mitre.org>
2017-04-05 17:12:44 -0400

    Add parsing of Malware objects

--- stix2/__init__.py
+++ stix2/__init__.py
@@ -2,9 +2,28 @@
 
 # flake8: noqa
 
+import json
+
 from .bundle import Bundle
 from .common import ExternalReference, KillChainPhase
 from .sdo import AttackPattern, Campaign, CourseOfAction, Identity, Indicator, \
     IntrusionSet, Malware, ObservedData, Report, ThreatActor, Tool, \
     Vulnerability
 from .sro import Relationship
+
+
+def parse(data):
+    """Deserialize a string or file-like object into a STIX object"""
+
+    try:
+        obj = json.loads(data)
+    except TypeError:
+        obj = json.load(data)
+
+    if 'type' not in obj:
+        # TODO parse external references, kill chain phases, and granular markings
+        pass
+    elif obj['type'] == 'malware':
+        return sdo.Malware(**obj)
+
+    return obj
