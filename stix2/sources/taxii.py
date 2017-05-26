2a8af45ec26c1c26de08721402be82207a87b53e
Emmanuelle Vargas-Gonzalez <emmanuelle@mitre.org>
2017-05-26 15:24:33 -0400

    Code style changes.

--- stix2/sources/taxii.py
+++ stix2/sources/taxii.py
@@ -3,27 +3,23 @@ from requests.auth import HTTPBasicAuth
 
 from stix2.sources import DataSource
 
-'''
-TODO:
+# TODO: -Should we make properties for the TAXIIDataSource address and other
+# possible variables that are found in "self.taxii_info"
 
--Should we make properties for the TAXIIDataSource address and other possible variables
-that are found in "self.taxii_info"
-'''
 
 TAXII_FILTERS = ['added_after', 'match[id]', 'match[type]', 'match[version]']
 
 
 class TAXIIDataSource(DataSource):
-    '''STIX 2.0 Data Source - TAXII 2.0 module'''
+    """STIX 2.0 Data Source - TAXII 2.0 module"""
 
-    def __init__(self, api_root=None, auth=None, name="TAXII", ):
-
+    def __init__(self, api_root=None, auth=None, name="TAXII"):
         super(TAXIIDataSource, self).__init__(name=name)
 
         self.taxii_info = {
             "api_root": {
-                            "url": api_root
-                        },
+                "url": api_root
+            },
             "auth": auth
         }
 
@@ -34,7 +30,8 @@ class TAXIIDataSource(DataSource):
 
             resp = requests.get(coll_url,
                                 headers=headers,
-                                auth=HTTPBasicAuth(self.taxii_info['auth']['user'], self.taxii_info['auth']['pass']))
+                                auth=HTTPBasicAuth(self.taxii_info['auth']['user'],
+                                                   self.taxii_info['auth']['pass']))
             # TESTING
             # print("\n-------__init__() ----\n")
             # print(resp.text)
@@ -53,8 +50,10 @@ class TAXIIDataSource(DataSource):
                 if e == "collections":
                     raise
                     # raise type(e), type(e)(e.message +
-                    # "To connect to the TAXII collections, the API root resource must contain a collection endpoint URL.
-                    # This was not found in the API root resource received from the API root" ), sys.exc_info()[2]
+                    # "To connect to the TAXII collections, the API root
+                    # resource must contain a collection endpoint URL.
+                    # This was not found in the API root resource received
+                    # from the API root" ), sys.exc_info()[2]
 
         except requests.ConnectionError as e:
             raise
@@ -62,20 +61,21 @@ class TAXIIDataSource(DataSource):
             #     "Attempting to connect to %s" % coll_url)
 
     def get(self, id_, _composite_filters=None):
-        '''get STIX 2 object from TAXII source by specified 'id'
+        """Get STIX 2.0 object from TAXII source by specified 'id'
 
-        NOTE:
-            -just pass _composite_filters to the query() as they are applied there
-            -deduplication of results is also done within query()
+        Notes:
+            Just pass _composite_filters to the query() as they are applied
+            there. de-duplication of results is also done within query()
 
         Args:
             id_ (str): id of STIX object to retrieve
 
-            _composite_filters (list): filters passed from a Composite Data Source (if this data source is attached to one)
+            _composite_filters (list): filters passed from a Composite Data
+                Source (if this data source is attached to one)
 
         Returns:
 
-        '''
+        """
 
         # make query in TAXII query format since 'id' is TAXii field
         query = [
@@ -94,20 +94,21 @@ class TAXIIDataSource(DataSource):
         return stix_obj
 
     def all_versions(self, id_, _composite_filters=None):
-        '''get all versions of STIX 2 object from TAXII source by specified 'id'
+        """Get all versions of STIX 2.0 object from TAXII source by
+        specified 'id'
 
-        NOTE:
-            -just passes _composite_filters to the query() as they are applied there
-            -deduplication of results is also done within query()
+        Notes:
+            Just passes _composite_filters to the query() as they are applied
+            there. de-duplication of results is also done within query()
 
         Args:
             id_ (str): id of STIX objects to retrieve
-
-            _composite_filters (list): filters passed from a Composite Data Source (if this data source is attached to one)
+            _composite_filters (list): filters passed from a Composite Data
+                Source (if this data source is attached to one)
 
         Returns:
-
-        '''
+            The query results with filters applied. 
+        """
 
         # make query in TAXII query format since 'id' is TAXII field
         query = [
@@ -123,15 +124,16 @@ class TAXIIDataSource(DataSource):
         return all_data
 
     def query(self, query=None, _composite_filters=None):
-        '''query the TAXII data source for STIX objects matching the query
+        """Query the TAXII data source for STIX objects matching the query
 
         The final full query could contain filters from:
             -the current API call
-            -Composite Data source filters (that are passed in via '_composite_filters')
+            -Composite Data source filters (that are passed in via
+                '_composite_filters')
             -TAXII data source filters that are attached
 
-        TAXII filters ['added_after', 'match[<>]'] are extracted and sent to TAXII
-        if they are present
+        TAXII filters ['added_after', 'match[<>]'] are extracted and sent
+        to TAXII if they are present
 
         TODO: Authentication for TAXII
 
@@ -139,12 +141,13 @@ class TAXIIDataSource(DataSource):
 
             query(list): list of filters (dicts) to search on
 
-            _composite_filters (list): filters passed from a Composite Data Source (if this data source is attached to one)
+            _composite_filters (list): filters passed from a
+                Composite Data Source (if this data source is attached to one)
 
         Returns:
 
 
-        '''
+        """
 
         all_data = []
 
@@ -157,24 +160,27 @@ class TAXIIDataSource(DataSource):
         if _composite_filters:
             query += _composite_filters
 
-        # seperate taxii query terms (can be done remotely)
+        # separate taxii query terms (can be done remotely)
         taxii_filters = self._parse_taxii_filters(query)
 
         # for each collection endpoint - send query request
         for collection in self.taxii_info['api_root']['collections']:
 
-            coll_obj_url = self.taxii_info['api_root']['url'] + "/collections/" + str(collection['id']) + "/objects/"
+            coll_obj_url = "/".join([self.taxii_info['api_root']['url'],
+                                     "collections", str(collection['id']),
+                                     "objects"])
             headers = {}
             try:
                 resp = requests.get(coll_obj_url,
                                     params=taxii_filters,
                                     headers=headers,
-                                    auth=HTTPBasicAuth(self.taxii_info['auth']['user'], self.taxii_info['auth']['pass']))
+                                    auth=HTTPBasicAuth(self.taxii_info['auth']['user'],
+                                                       self.taxii_info['auth']['pass']))
                 # TESTING
                 # print("\n-------query() ----\n")
                 # print("Request that was sent: \n")
                 # print(resp.url)
-                # print("Reponse: \n")
+                # print("Response: \n")
                 # print(json.dumps(resp.json(),indent=4))
                 # print("\n")
                 # print(resp.status_code)
@@ -194,9 +200,8 @@ class TAXIIDataSource(DataSource):
                 # raise type(e), type(e)(e.message +
                 # "Attempting to connect to %s" % coll_url)
 
-            '''
-            TODO: Is there a way to collect exceptions while carrying on then raise all of them at the end?
-            '''
+            # TODO: Is there a way to collect exceptions while carrying
+            # on then raise all of them at the end?
 
         # deduplicate data (before filtering as reduces wasted filtering)
         all_data = self.deduplicate(all_data)
@@ -207,24 +212,25 @@ class TAXIIDataSource(DataSource):
         return all_data
 
     def _parse_taxii_filters(self, query):
-        '''parse out TAXII filters that the TAXII server can filter on
+        """Parse out TAXII filters that the TAXII server can filter on
 
         TAXII filters should be analgous to how they are supplied
         in the url to the TAXII endpoint. For instance
         "?match[type]=indicator,sighting" should be in a query dict as follows
         {
-            "field":"match[type]"
+            "field": "match[type]"
             "op": "=",
-            "value":"indicator,sighting"
+            "value": "indicator,sighting"
         }
 
         Args:
-            query (list): list of filters to extract which ones are TAXII specific
+            query (list): list of filters to extract which ones are TAXII
+                specific.
 
         Returns:
-            params (dict): dict of the TAXII filters but in format required for 'requests.get()'
-
-        '''
+            params (dict): dict of the TAXII filters but in format required
+                for 'requests.get()'.
+        """
 
         params = {}
 
@@ -234,12 +240,9 @@ class TAXIIDataSource(DataSource):
         return params
 
     def close(self):
-        '''close down the Data Source - if any clean up is required
+        """Close down the Data Source - if any clean up is required.
 
-        '''
+        """
         pass
 
-    '''
-    TODO:
-        - getters/setters (properties) for TAXII config info
-    '''
+    # TODO: - getters/setters (properties) for TAXII config info
