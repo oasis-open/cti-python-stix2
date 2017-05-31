9dbde930314d11f4185d3613412683d0b2a88714
Greg Back <gback@mitre.org>
2017-05-31 10:03:50 -0500

    Bump version: 0.1.0 → 0.2.0

--- docs/conf.py
+++ docs/conf.py
@@ -9,8 +9,8 @@ project = 'stix2'
 copyright = '2017, OASIS Open'
 author = 'OASIS Open'
 
-version = '0.1.0'
-release = '0.1.0'
+version = '0.2.0'
+release = '0.2.0'
 
 language = None
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
