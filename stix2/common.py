228f488f5bde687eb439ac26dd52cb257922c19b
Emmanuelle Vargas-Gonzalez <emmanuelle@mitre.org>
2017-08-11 15:11:54 -0400

    Update commons.py to new OrderedDict format, just in case.

--- stix2/common.py
+++ stix2/common.py
@@ -1,18 +1,22 @@
 """STIX 2 Common Data Types and Properties"""
 
+from collections import OrderedDict
+
 from .other import ExternalReference, GranularMarking
 from .properties import (BooleanProperty, ListProperty, ReferenceProperty,
                          StringProperty, TimestampProperty)
 from .utils import NOW
 
-COMMON_PROPERTIES = {
+COMMON_PROPERTIES = OrderedDict()
+
+COMMON_PROPERTIES.update([
     # 'type' and 'id' should be defined on each individual type
-    'created': TimestampProperty(default=lambda: NOW, precision='millisecond'),
-    'modified': TimestampProperty(default=lambda: NOW, precision='millisecond'),
-    'external_references': ListProperty(ExternalReference),
-    'revoked': BooleanProperty(),
-    'labels': ListProperty(StringProperty),
-    'created_by_ref': ReferenceProperty(type="identity"),
-    'object_marking_refs': ListProperty(ReferenceProperty(type="marking-definition")),
-    'granular_markings': ListProperty(GranularMarking),
-}
+    ('created_by_ref', ReferenceProperty(type="identity")),
+    ('created', TimestampProperty(default=lambda: NOW, precision='millisecond')),
+    ('modified', TimestampProperty(default=lambda: NOW, precision='millisecond')),
+    ('revoked', BooleanProperty()),
+    ('labels', ListProperty(StringProperty)),
+    ('external_references', ListProperty(ExternalReference)),
+    ('object_marking_refs', ListProperty(ReferenceProperty(type="marking-definition"))),
+    ('granular_markings', ListProperty(GranularMarking)),
+])
