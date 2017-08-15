f67d34990c7105d4db83d791e7b851ae730ac556
Emmanuelle Vargas-Gonzalez <emmanuelle@mitre.org>
2017-08-15 14:16:49 -0400

    Style error.

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
     keywords="stix stix2 json cti cyber threat intelligence",
     packages=find_packages(),
     install_requires=[
-        'ordereddict'
+        'ordereddict',
         'python-dateutil',
         'pytz',
         'requests',
