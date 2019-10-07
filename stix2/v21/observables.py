2be70131d7f10d39969482c15c7ba8e9da5dce4e
Desai, Kartikey H <khdesai@mitre.org>
2019-10-07 13:04:15 -0400

    Fixes #296

--- stix2/v21/observables.py
+++ stix2/v21/observables.py
@@ -385,7 +385,7 @@ class File(_Observable):
         ('mtime', TimestampProperty()),
         ('atime', TimestampProperty()),
         ('parent_directory_ref', ReferenceProperty(valid_types='directory', spec_version='2.1')),
-        ('contains_refs', ListProperty(ReferenceProperty(invalid_types="", spec_version='2.1'))),
+        ('contains_refs', ListProperty(ReferenceProperty(invalid_types=[""], spec_version='2.1'))),
         ('content_ref', ReferenceProperty(valid_types='artifact', spec_version='2.1')),
         ('extensions', ExtensionsProperty(spec_version='2.1', enclosing_type=_type)),
         ('spec_version', StringProperty(fixed='2.1')),
