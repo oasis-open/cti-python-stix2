46219bf072da0b4d11014ce62565dc61c68d603f
Rich Piazza <rpiazza@mitre.org>
2020-03-25 11:36:29 -0400

    add 2.1 links

--- stix2/v21/common.py
+++ stix2/v21/common.py
@@ -16,9 +16,8 @@ from ..utils import NOW, _get_dict
 
 
 class ExternalReference(_STIXBase):
-    # TODO: Add link
     """For more detailed information on this object's properties, see
-    `the STIX 2.1 specification <link here>`__.
+    `the STIX 2.1 specification <https://docs.oasis-open.org/cti/stix/v2.1/cs01/stix-v2.1-cs01.html#_bajcvqteiard>`__.
     """
 
     _properties = OrderedDict([
@@ -51,9 +50,8 @@ class ExternalReference(_STIXBase):
 
 
 class KillChainPhase(_STIXBase):
-    # TODO: Add link
     """For more detailed information on this object's properties, see
-    `the STIX 2.1 specification <link here>`__.
+    `the STIX 2.1 specification <https://docs.oasis-open.org/cti/stix/v2.1/cs01/stix-v2.1-cs01.html#_i4tjv75ce50h>`__.
     """
 
     _properties = OrderedDict([
@@ -63,9 +61,8 @@ class KillChainPhase(_STIXBase):
 
 
 class GranularMarking(_STIXBase):
-    # TODO: Add link
     """For more detailed information on this object's properties, see
-    `the STIX 2.1 specification <link here>`__.
+    `the STIX 2.1 specification <https://docs.oasis-open.org/cti/stix/v2.1/cs01/stix-v2.1-cs01.html#_robezi5egfdr>`__.
     """
 
     _properties = OrderedDict([
@@ -80,9 +77,8 @@ class GranularMarking(_STIXBase):
 
 
 class LanguageContent(_STIXBase):
-    # TODO: Add link
     """For more detailed information on this object's properties, see
-    `the STIX 2.1 specification <link here>`__.
+    `the STIX 2.1 specification <https://docs.oasis-open.org/cti/stix/v2.1/cs01/stix-v2.1-cs01.html#_nfwr8z9ax2bi>`__.
     """
 
     _type = 'language-content'
@@ -108,9 +104,8 @@ class LanguageContent(_STIXBase):
 
 
 class TLPMarking(_STIXBase):
-    # TODO: Add link
     """For more detailed information on this object's properties, see
-    `the STIX 2.1 specification <link here>`__.
+    `the STIX 2.1 specification <https://docs.oasis-open.org/cti/stix/v2.1/cs01/stix-v2.1-cs01.html#_yd3ar14ekwrs>`__.
     """
 
     _type = 'tlp'
@@ -120,9 +115,8 @@ class TLPMarking(_STIXBase):
 
 
 class StatementMarking(_STIXBase):
-    # TODO: Add link
     """For more detailed information on this object's properties, see
-    `the STIX 2.1 specification <link here>`__.
+    `the STIX 2.1 specification <https://docs.oasis-open.org/cti/stix/v2.1/cs01/stix-v2.1-cs01.html#_3ru8r05saera>`__.
     """
 
     _type = 'statement'
@@ -151,9 +145,8 @@ class MarkingProperty(Property):
 
 
 class MarkingDefinition(_STIXBase, _MarkingsMixin):
-    # TODO: Add link
     """For more detailed information on this object's properties, see
-    `the STIX 2.1 specification <link here>`__.
+    `the STIX 2.1 specification <https://docs.oasis-open.org/cti/stix/v2.1/cs01/stix-v2.1-cs01.html#_hr5vgqxjk7ns>`__.
     """
 
     _type = 'marking-definition'
