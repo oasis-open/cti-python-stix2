cada2687e072323b4b62e8ef3b54c6f00c40ec14
Gage Hackford <ghackford@mitre.org>
2024-04-07 20:49:37 -0400

    adding postgres authentication and fixing store.get bug

--- stix2/test/v21/test_url.py
+++ stix2/test/v21/test_url.py
@@ -8,12 +8,12 @@ import stix2.properties
 
 url_dict = {
     "type": "url",
-    "id": "url--c1477287-23ac-5971-a010-5c287877fa60",
+    "id": "url--a5477287-23ac-5971-a010-5c287877fa60",
     "value" : "https://example.com/research/index.html"
 }
 
 store = RelationalDBStore(
-        "postgresql://localhost/stix-data-sink",
+        "postgresql://postgres:admin@localhost/postgres",
         False,
         None,
         True
@@ -23,7 +23,7 @@ def test_url():
     store.sink.generate_stix_schema()
     url_stix_object = stix2.parse(url_dict)
     store.add(url_stix_object)
-    read_obj = json.loads(store.get(url_stix_object).serialize())
+    read_obj = json.loads(store.get(url_stix_object['id']).serialize())
 
     for attrib in url_dict.keys():
         assert url_dict[attrib] == read_obj[attrib]
