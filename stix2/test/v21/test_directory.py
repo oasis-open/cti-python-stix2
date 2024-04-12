4ef83608248749762d5d039895720ff47fae4ea3
Gage Hackford <ghackford@mitre.org>
2024-04-12 13:25:08 -0400

    updating directory test with ctime and mtime check

--- stix2/test/v21/test_directory.py
+++ stix2/test/v21/test_directory.py
@@ -4,11 +4,12 @@ import json
 
 from stix2.datastore.relational_db.relational_db import RelationalDBStore
 
-# Ctime and mtime will return as stix2 objects from the DB get function so comparison doesn't work for those
 directory_dict = {
     "type": "directory",
     "spec_version": "2.1",
-    "id": "directory--67c0a9b0-520d-545d-9094-1a08ddf46b05",
+    "id": "directory--17c909b1-521d-545d-9094-1a08ddf46b05",
+    "ctime": "2018-11-23T08:17:27.000Z",
+    "mtime": "2018-11-23T08:17:27.000Z",
     "path": "C:\\Windows\\System32",
     "path_enc": "cGF0aF9lbmM",
     "contains_refs": [
@@ -33,4 +34,8 @@ def test_directory():
     for attrib in directory_dict.keys():
         if attrib == "contains_refs": # TODO remove skip once we can pull from table join
             continue
+        if attrib == "ctime" or attrib == "mtime": # convert both into stix2 date format for consistency
+            assert stix2.utils.parse_into_datetime(directory_dict[attrib]) == stix2.utils.parse_into_datetime(read_obj[attrib])
+            continue
         assert directory_dict[attrib] == read_obj[attrib]
+
