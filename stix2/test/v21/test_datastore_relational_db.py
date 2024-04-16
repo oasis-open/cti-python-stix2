1583e4de370e98c57585bc3ed6e0cdb02c12995c
Rich Piazza <rpiazza@mitre.org>
2024-04-16 15:43:05 -0400

    change postgres url

--- stix2/test/v21/test_datastore_relational_db.py
+++ stix2/test/v21/test_datastore_relational_db.py
@@ -5,7 +5,7 @@ from stix2.datastore.relational_db.relational_db import RelationalDBStore
 import stix2.properties
 
 store = RelationalDBStore(
-    "postgresql://localhost/stix-data-sink",
+    "postgresql://localhost/postgres",
     True,
     None,
     False,
