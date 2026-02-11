d015719e16b989ed160d1fef60111a14ae0ae365
Christian Studer <christian.studer@circl.lu>
2026-02-11 09:04:41 +0100

    fix: Fixed `stix2-patterns` dependency version requirement

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setup(
         'pytz',
         'requests',
         'simplejson',
-        'stix2-patterns>=1.2.0',
+        'stix2-patterns>=2.1.1',
     ],
     project_urls={
         'Documentation': 'https://stix2.readthedocs.io/',
